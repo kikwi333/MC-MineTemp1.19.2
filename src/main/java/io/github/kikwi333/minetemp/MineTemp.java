d322499c0a7696700b61da6250519229f6715a55
kikwi333 <63129127+kikwi333@users.noreply.github.com>
2022-10-03 01:13:11 -0400

    Mod setup

---
+++ src/main/java/io/github/kikwi333/minetemp/MineTemp.java
@@ -0,0 +1,8 @@
+package io.github.kikwi333.minetemp;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MineTemp.MODID)
+public class MineTemp {
+    public static final String MODID = "minetemp";
+}
